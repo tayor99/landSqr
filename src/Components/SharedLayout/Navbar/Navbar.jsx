2af555db93b6667bb06da756406fb39111d3fda8
tayor <adenusiadetayo@gmail.com>
2022-10-15 16:10:20 -0700

    added navbar

--- src/Components/SharedLayout/Navbar/Navbar.jsx
+++ src/Components/SharedLayout/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import "./navbar.scss";
-
+import { AiOutlineSearch } from "react-icons/ai";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import profileImg from "../../../Assets/image 4.png";
 
 const Navbar = () => {
   return (
@@ -8,9 +10,34 @@ const Navbar = () => {
         <div className="navbar__searchbar">
           <form className="searchbar">
             <div className="form-details">
-              <input type="text" />
+              <input type="text" placeholder="Search for anything" />
+              <button>
+                <AiOutlineSearch />
+              </button>
             </div>
           </form>
+        </div>
+        <div className="navbar__links">
+          <ul>
+            <li>
+              <p className="docs">Docs</p>
+            </li>
+            <li>
+              <p className="bell">
+                <IoIosNotificationsOutline />
+              </p>
+            </li>
+            <li>
+              {/* <div className="img__container"> */}
+              <img
+                className="img__container"
+                src={profileImg}
+                alt="profile img"
+              />
+              {/* </div> */}
+              <p className="username">Adedeji</p>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
